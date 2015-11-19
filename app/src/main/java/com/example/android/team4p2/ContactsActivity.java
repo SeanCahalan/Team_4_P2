9464ee1c044851ef53cdd41c7d4cc5347ea1358c
Troy Wolters <TW80000@gmail.com>
2015-11-18 23:55:36 -0500

    Added "add contact to Android" functionality

--- app/src/main/java/com/example/android/team4p2/ContactsActivity.java
+++ app/src/main/java/com/example/android/team4p2/ContactsActivity.java
@@ -2,25 +2,26 @@ package com.example.android.team4p2;
 
 import android.content.ActivityNotFoundException;
 import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.Loader;
 import android.content.OperationApplicationException;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.RemoteException;
+import android.provider.Contacts;
 import android.provider.ContactsContract;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
-import android.speech.tts.TextToSpeech.OnInitListener;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -31,7 +32,6 @@ public class ContactsActivity extends AppCompatActivity implements TextToSpeech.
     private TextToSpeech tts;
     private String text;
     private Button btn;
-    // private ContentResolver cr = new ContentResolver(getApplicationContext()) {};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +51,31 @@ public class ContactsActivity extends AppCompatActivity implements TextToSpeech.
         Intent intent = getIntent();
         String user_input = intent.getStringExtra(MainActivity.USER_INPUT);
 
-        // Code here if need be...
+        // Code here using the user input from the main activity if need be...
+
+        tts = new TextToSpeech(this, this);
+        btn = (Button) findViewById(R.id.btn);
+
+        // This is a button to playback the string right now. Eventually this needs
+        // to be replaced with voice commands.
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                speakOut();
+                addContact(text, "adolf@fake.com", "6131234567");
+                while (tts.isSpeaking()) {}
+                say("Added " + text + " to contacts.");
+            }
+        });
+    }
+
+    /*
+     *  This is the "top" of the process, where we begin to interpret the commands.
+     */
+    private void handleCommand() {
+        // use the variable `text` in this function to process commands.
+
+
     }
 
     private void promptSpeechInput() {
@@ -72,7 +96,7 @@ public class ContactsActivity extends AppCompatActivity implements TextToSpeech.
 
     /**
      * Receiving speech input
-     * */
+     **/
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -83,32 +107,11 @@ public class ContactsActivity extends AppCompatActivity implements TextToSpeech.
                     ArrayList<String> result = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     text = result.get(0);
-
-                    tts = new TextToSpeech(this, this);
-                    btn = (Button) findViewById(R.id.btn);
-
-                    // This is a button to playback the string right now. Eventually this needs
-                    // to be replaced with voice commands.
-                    btn.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View arg0) {
-                            speakOut();
-                            // insertContact(cr, "Adolf", "6133923445");
-                        }
-                    });
-
-//                    btnSpeak.setOnClickListener(new View.OnClickListener() {
-//                        @Override
-//                        public void onClick(View arg0) {
-//                            speakOut();
-//                        }
-//                    });
-
+                    handleCommand();
                 }
                 break;
             }
         }
-
     }
 
     @Override
@@ -135,5 +138,113 @@ public class ContactsActivity extends AppCompatActivity implements TextToSpeech.
 
     private void speakOut() {
         tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "aslkjfds");
+    }
+
+    private void say(String str) {
+        tts.speak(str, TextToSpeech.QUEUE_FLUSH, null, "asdfsdfsd");
+    }
+
+    private void addContact(String name, String email, String phone) {
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+        int rawContactInsertIndex = ops.size();
+
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null).build());
+
+        //Phone Number
+        ops.add(ContentProviderOperation
+                .newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,
+                        rawContactInsertIndex)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, phone)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, "1").build());
+
+        //Display name/Contact name
+        ops.add(ContentProviderOperation
+                .newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,
+                        rawContactInsertIndex)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, name)
+                .build());
+        //Email details
+        ops.add(ContentProviderOperation
+                .newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,
+                        rawContactInsertIndex)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Email.DATA, email)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Email.TYPE, "2").build());
+
+
+        //Postal Address
+
+        ops.add(ContentProviderOperation
+                .newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,
+                        rawContactInsertIndex)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredPostal.POBOX, "Postbox")
+
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredPostal.STREET, "street")
+
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredPostal.CITY, "city")
+
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredPostal.REGION, "region")
+
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredPostal.POSTCODE, "postcode")
+
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredPostal.COUNTRY, "country")
+
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredPostal.TYPE, "3")
+
+                .build());
+
+
+        //Organization details
+        ops.add(ContentProviderOperation
+                .newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,
+                        rawContactInsertIndex)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE )
+                .withValue(ContactsContract.CommonDataKinds.Organization.COMPANY, "Devindia")
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE )
+                .withValue(ContactsContract.CommonDataKinds.Organization.TITLE, "Developer")
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Organization.CONTENT_ITEM_TYPE )
+                .withValue(ContactsContract.CommonDataKinds.Organization.TYPE, "0")
+
+                .build());
+        //IM details
+        ops.add(ContentProviderOperation
+                .newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,
+                        rawContactInsertIndex)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Im.DATA, "adolf123")
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE )
+                .withValue(ContactsContract.CommonDataKinds.Im.DATA5, "2")
+
+
+                .build());
+        try {
+            ContentProviderResult[] res = getContentResolver().applyBatch(
+                    ContactsContract.AUTHORITY, ops);
+        } catch (RemoteException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (OperationApplicationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }

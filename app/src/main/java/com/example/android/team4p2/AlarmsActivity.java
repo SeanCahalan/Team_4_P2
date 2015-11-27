06875b7ee3f2e8a9c5b9fbc5ab02a0f2011888c2
Troy Wolters <TW80000@gmail.com>
2015-11-27 16:52:57 -0500

    Added command options to "make alarm". Code cleanup.

--- app/src/main/java/com/example/android/team4p2/AlarmsActivity.java
+++ app/src/main/java/com/example/android/team4p2/AlarmsActivity.java
@@ -22,6 +22,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
+import java.util.regex.Pattern;
 
 import static java.lang.Integer.parseInt;
 
@@ -43,7 +44,7 @@ public class AlarmsActivity extends AppCompatActivity implements TextToSpeech.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarms);
 
-        setup();
+        initializeAlarmInfrastructure();
 
         btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
         btnSpeak.setOnClickListener(new View.OnClickListener() {
@@ -56,7 +57,7 @@ public class AlarmsActivity extends AppCompatActivity implements TextToSpeech.On
 
         tts = new TextToSpeech(this, this);
 
-        // Set up `available_commands` for help command
+        // Initialize the `available_commands` variable used by the help command
         for (String command: commands) {
             available_commands = available_commands.concat(command + ", ");
         }
@@ -68,9 +69,11 @@ public class AlarmsActivity extends AppCompatActivity implements TextToSpeech.On
      *  This is the "top" of the process, where we begin to interpret the commands.
      */
     private void handleCommand() {
-        // At this point a command has been issued and stored in the variable `text`
+        // At this point a command has already been issued and stored in the variable `text`
         String nText = MainActivity.normalizeCommand(text);
         String command = extractCommand(nText);
+        if (command == null)
+            command = "help";
         switch (command) {
             case "delete": {
                 CURRENT_PROCESS = "contact-delete-name";
@@ -101,7 +104,6 @@ public class AlarmsActivity extends AppCompatActivity implements TextToSpeech.On
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         switch (requestCode) {
             case MainActivity.REQ_CODE_SPEECH_INPUT: {
                 if (resultCode == RESULT_OK && null != data) {
@@ -118,9 +120,17 @@ public class AlarmsActivity extends AppCompatActivity implements TextToSpeech.On
     private void nextProcess() {
         switch (CURRENT_PROCESS) {
             case "alarm-make": {
-                CURRENT_PROCESS = "DEFAULT";
-                addAlarm();
-                say("Successfully added your alarm for " + text);
+                if (text.equalsIgnoreCase("cancel")) {
+                    say("Cancelling.");
+                    CURRENT_PROCESS = "DEFAULT";
+                } else if (Pattern.matches("\\d?\\d:\\d\\d\\s[ap].m.", text.toLowerCase())) {
+                    CURRENT_PROCESS = "DEFAULT";
+                    addAlarm();
+                    say("Successfully added your alarm for " + text);
+                } else {
+                    say("That is not a valid time. Remember to specify A.M. or P.M. Please try again.");
+                    promptSpeechInput();
+                }
                 break;
             }
             default: {
@@ -132,12 +142,8 @@ public class AlarmsActivity extends AppCompatActivity implements TextToSpeech.On
 
     private void addAlarm() {
 
-        // int current_hour = cal.get(Calendar.HOUR_OF_DAY);
         int user_hour = getHour(text);
-        // int current_minutes = cal.get(Calendar.MINUTE);
         int user_minutes = getMinutes(text);
-        // long needed_time = ((24 - Math.abs(user_hour - current_hour) * 60 + (60 - Math.abs(user_minutes - current_minutes)))) * 60 * 1000;
-        // long final_time = System.currentTimeMillis() + needed_time;
         Boolean user_isPm = getIsPM(text);
 
         if (user_hour == 12 && !user_isPm)
@@ -145,11 +151,6 @@ public class AlarmsActivity extends AppCompatActivity implements TextToSpeech.On
         else if (user_hour < 12 && user_isPm) {
             user_hour += 12;
         }
-
-        Log.d("TROY", "user_hour: " + user_hour);
-        Log.d("TROY", "user_minutes: " + user_minutes);
-
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.CANADA);
 
         // Initialize a new calendar with the right info (don't pass it anything)
         Calendar cal = new GregorianCalendar();
@@ -161,9 +162,6 @@ public class AlarmsActivity extends AppCompatActivity implements TextToSpeech.On
         int hours_to_add = Math.abs(user_hour - cal.get(Calendar.HOUR_OF_DAY));
         int minutes_to_add = Math.abs(user_minutes - cal.get(Calendar.MINUTE));
 
-        Log.d("TROY", "hours_to_add: " + hours_to_add);
-        Log.d("TROY", "minutes_to_add: " + minutes_to_add);
-
         // Add them to the calendar
         cal.add(Calendar.HOUR_OF_DAY, hours_to_add);
         cal.add(Calendar.MINUTE, minutes_to_add);
@@ -171,8 +169,6 @@ public class AlarmsActivity extends AppCompatActivity implements TextToSpeech.On
 
         // Get the new calendar time in milliseconds
         long final_time = cal.getTimeInMillis();
-        String alarmTime = sdf.format(final_time);
-        Log.d("TROY", alarmTime);
 
         AlarmManager.AlarmClockInfo alarm_info = new AlarmManager.AlarmClockInfo(final_time, pi);
         am.setAlarmClock(alarm_info, pi);
@@ -240,17 +236,12 @@ public class AlarmsActivity extends AppCompatActivity implements TextToSpeech.On
         }
     }
 
-    private void setup() {
+    private void initializeAlarmInfrastructure() {
         br = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 Intent it = new Intent(getApplicationContext(), audioService.class);
                 it.setAction("com.example.android.team4p2");
-                Toast.makeText(
-                        getApplicationContext(),
-                        "JOHHHHHHNNNNNNN CENAAAAAAA",
-                        Toast.LENGTH_LONG
-                ).show();
                 startService(it);
             }
         };
